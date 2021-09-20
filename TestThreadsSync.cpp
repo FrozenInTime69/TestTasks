ab575b7f6aa2094c84f616e96ca8e0e0028f2882
FrozenInTime69 <66874504+FrozenInTime69@users.noreply.github.com>
2021-09-21 01:13:58 +0300

    Add files via upload

---
+++ TestThreadsSync.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+
+using namespace std;
+mutex mu;
+condition_variable cond;
+bool ready = true;
+
+void print_first()
+{
+	while (true)
+	{
+		unique_lock<mutex> locker(mu);
+		cond.wait(locker, []{return ready;});
+		cout << "1" << "\n";
+		//std::this_thread::sleep_for(std::chrono::seconds(3));
+		locker.unlock();
+		cond.notify_one();
+		ready = false;
+	}
+}
+
+void print_second()
+{
+	while (true)
+	{
+		unique_lock<mutex> locker(mu);
+		cond.wait(locker, [] {return !ready; });
+		cout << "2" << "\n";
+		//std::this_thread::sleep_for(std::chrono::seconds(2));
+		locker.unlock();
+		cond.notify_one();
+		ready = true;
+	}
+}
+int main()
+{
+	thread t1(print_first);
+	thread t2(print_second);
+	t1.join();
+	t2.join();
+	return 0;
+}
